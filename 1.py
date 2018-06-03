6ee97dccb0b7f9bb77b4c45d036e7216337389f5
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-03 22:31:51 +0530

    ifelse

---
+++ 1.py
@@ -0,0 +1,45 @@
+print("menu")
+print("(1) +")
+print("(2) -")
+print("(3) *")
+print("(4) /")
+print("(5) %")
+x=int(input("enter the value"))
+
+
+if x==1 :
+
+    print("enter two values")
+    a=int(input("1--"))
+    b=int(input("2--"))
+    print(a+b)
+
+elif x==2 :
+
+    print("enter two values")
+    a=int(input("1--"))
+    b=int(input("2--"))
+    print(a-b)
+
+elif x==3 :
+
+    print("enter two values")
+    a=int(input("1--"))
+    b=int(input("2--"))
+    print(a*b)
+
+elif x==4 :
+
+    print("enter two values")
+    a=int(input("1--"))
+    b=int(input("2--"))
+    print(a/b)
+
+elif x==5:
+
+    print("enter two values")
+    a=int(input("1--"))
+    b=int(input("2--"))
+    print(a%b)
+
+
---
+++ 3.py
@@ -0,0 +1,18 @@
+marks = int(input('enter your marks'))
+
+if marks == 100:
+    print('Grade A+')
+elif marks >= 90:
+    print('Grade A')
+elif marks >= 80:
+    print('Grade B+')
+elif marks >= 70:
+    print('Grade B')
+elif marks >= 60:
+    print('Grade C')
+elif marks >= 50:
+    print('Grade D')
+elif marks >=30:
+    print('Grade E')
+elif marks < 30:
+    print('Grade F')
